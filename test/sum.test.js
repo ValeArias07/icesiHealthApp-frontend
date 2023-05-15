47a1b589236a4254cb9a0a3392228416fd71424b
ValeArias07 <valeariasparra2001@gmail.com>
2023-05-14 20:45:06 -0500

    add test class

---
+++ test/sum.test.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+
+describe('sum function', () => {
+  it('should add two numbers correctly', () => {
+    assert.strictEqual(5, 5);
+  });
+
+  it('should return NaN if either argument is not a number', () => {
+    const result = 3;
+    assert.strictEqual(5, 5);
+  });
+});

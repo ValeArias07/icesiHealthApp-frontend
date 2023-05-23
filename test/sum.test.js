78684bca542c76a916f1d47901c8400ebd6c8a05
Valentina Arias Parra <54784992+ValeArias07@users.noreply.github.com>
2023-05-23 17:34:22 -0500

    Update sum.test.js

--- test/sum.test.js
+++ test/sum.test.js
@@ -2,11 +2,18 @@ const assert = require('assert');
 
 describe('sum function', () => {
   it('should add two numbers correctly', () => {
-    assert.strictEqual(5, 5);
+    let result = 0;
+    let number1= 3;
+    let number2= 2;
+    result = number1 + number2;
+    assert.strictEqual(result, 5);
   });
 
-  it('should return NaN if either argument is not a number', () => {
-    const result = 3;
-    assert.strictEqual(5, 5);
+  it('should multiply two numbers correctly', () => {
+    let result = 0;
+    let number1= 5;
+    let number2= 2;
+    result = number1 * number2;
+    assert.strictEqual(result, 10);
   });
 });
